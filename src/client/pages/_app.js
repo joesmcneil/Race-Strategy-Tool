5bb6f8edb1eba58b23b2926ada2066d5704fba08
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2023-01-15 20:59:32 +0000

    Small changes with added scss global file and import

--- src/client/pages/_app.js
+++ src/client/pages/_app.js
@@ -2,6 +2,9 @@
 
 // import Head from 'next/head';
 
+import React from "react";
+// import "../styles/styles.scss";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
