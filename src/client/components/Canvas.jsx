4063e7ab4f4cb301bdda78fb05ed934b106e8da3
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2023-01-25 19:08:09 +0000

    Added more canvas bits

--- src/client/components/Canvas.jsx
+++ src/client/components/Canvas.jsx
@@ -8,14 +8,40 @@ const Canvas = props => {
     const [height, setHeight] = useState(0);
     const [width, setWidth] = useState(0);
 
+    const racePath = {
+        x:500,
+        y:500,
+        r:200
+    }
+
     const draw = ctx => {
 
         // track
         ctx.beginPath();
-        ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+        ctx.arc(500, 500, 480, 0, 2 * Math.PI);
         ctx.strokeStyle = 'white';
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 15;
         ctx.stroke();
+
+        // timing circle
+        ctx.beginPath();
+        ctx.arc(500, 500, 200, 0, 2 * Math.PI);
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = 10;
+        ctx.stroke();
+
+        // racer 1
+        ctx.beginPath();
+        ctx.arc(325, 50, 35, 0, 2 * Math.PI);
+        ctx.fillStyle = 'blue';
+        ctx.fill();
+
+        // racer 2
+        ctx.beginPath();
+        ctx.arc(650, 50, 35, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+
       }
 
     useEffect(() => {
