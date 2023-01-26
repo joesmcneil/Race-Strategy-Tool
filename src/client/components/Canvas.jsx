da6e2ded83bcd6813dd533fa71557b576c488456
UP2020033 <72713194+UP2020033@users.noreply.github.com>
2023-01-26 22:25:06 +0000

    Added functionality to move car on track

--- src/client/components/Canvas.jsx
+++ src/client/components/Canvas.jsx
@@ -1,51 +1,73 @@
 import React, { useState, useRef, useEffect } from 'react'
-import {cover, contain} from 'intrinsic-scale';
 
 const Canvas = props => {
 
-    const canvasRef = useRef(null)
-
+    const canvasRef = useRef(null);
     const [height, setHeight] = useState(0);
     const [width, setWidth] = useState(0);
+    const [time, setTime]= useState(0);
+    const radius = 480;
 
-    const racePath = {
-        x:500,
-        y:500,
-        r:200
-    }
+    // useEffect called on mount to update time by 5 every 16 ms.
+    // code initially in component code rather than on mount, meaning interval was being reset therefore resetting the value of time.
 
-    const draw = ctx => {
+    useEffect(() => {
+        setInterval( () => {
+            setTime(time => time + 5)
+        }, 16)
+    }, [])
 
-        // track
+    const drawTrack = ctx => {
+        // Track
         ctx.beginPath();
-        ctx.arc(500, 500, 480, 0, 2 * Math.PI);
-        ctx.strokeStyle = 'white';
+        ctx.arc(500, 500, radius, 0, 2 * Math.PI);
+        ctx.strokeStyle = 'lightgreen';
         ctx.lineWidth = 15;
         ctx.stroke();
 
-        // timing circle
+        // Timing circle
         ctx.beginPath();
         ctx.arc(500, 500, 200, 0, 2 * Math.PI);
-        ctx.strokeStyle = 'white';
+        ctx.strokeStyle = 'lightblue';
         ctx.lineWidth = 10;
         ctx.stroke();
+    }
 
-        // racer 1
+    function drawRacer (ctx, colour) {
+
+        const circumference = 2 * Math.PI * radius;
+        const position = time % circumference;
+        const degrees = position * 360 / circumference;
+
+        // Note that the addition of the radius and (35 + 2) (radius of the car)
+        // is used as an offset to center the car on the track assuming the track
+        // is a circle and the center of the circle is at w/2, h/2.. This could be
+        // rectified by adding an offset of the origin of the circle 
+        const offset = radius + (35 / 2);
+        const x = (radius * Math.sin(Math.PI * 2 * degrees / 360)) + offset;
+        const y = (radius * Math.cos(Math.PI * 2 * degrees / 360)) + offset;
+
         ctx.beginPath();
-        ctx.arc(325, 50, 35, 0, 2 * Math.PI);
-        ctx.fillStyle = 'blue';
-        ctx.fill();
+        ctx.moveTo(canvasRef.current.width / 2, canvasRef.current.height / 2);
+        ctx.lineTo(x, y);
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = colour;
+        ctx.stroke();
 
-        // racer 2
         ctx.beginPath();
-        ctx.arc(650, 50, 35, 0, 2 * Math.PI);
-        ctx.fillStyle = 'red';
-        ctx.fill();
+        ctx.arc(x, y, 35, 0, 2 * Math.PI);
+        ctx.fillStyle = colour;
+        ctx.fill();   
+    }
 
-      }
+    const draw = ctx => {
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+
+        drawTrack(ctx);
+        drawRacer(ctx, 'pink');
+    }
 
     useEffect(() => {
-
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
@@ -53,12 +75,9 @@ const Canvas = props => {
         setWidth(canvasRef.current.parentElement.offsetWidth);
 
         draw(context);
-    }, [draw, height, width]);
+    }, [draw, height, width, time]);
 
     return <canvas ref={canvasRef} height={height} width={width} {...props}/>
-
 }
-
-
 
 export default Canvas
